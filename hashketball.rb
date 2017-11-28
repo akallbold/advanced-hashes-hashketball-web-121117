217d20f9a2d7e5c4046bd2dd2b88d26103cbbd8b
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-28 19:44:28 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,7 +192,7 @@ def player_numbers(team)
               # if value == team
 # output << key2
               output << stat_value
-            end
+            # end
             end
           end
         end
